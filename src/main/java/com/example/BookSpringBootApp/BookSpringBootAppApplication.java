69b9eaf114e2562d2474061a0d253004b0865e52
Christiaan Smits <150244385+ChrisCzsSCHOOL@users.noreply.github.com>
2024-09-17 00:52:36 +0200

    AAA

---
+++ src/main/java/com/example/BookSpringBootApp/BookSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookSpringBootApp/BookSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookSpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
