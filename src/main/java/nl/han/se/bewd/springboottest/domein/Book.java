bb9a6c6850e05f2e007b495a639e1bdf888b9de8
Christiaan Smits <150244385+ChrisCzsSCHOOL@users.noreply.github.com>
2024-09-17 13:50:23 +0200

    In de les werkend

--- src/main/java/nl/han/se/bewd/springboottest/domein/Book.java
+++ src/main/java/nl/han/se/bewd/springboottest/domein/Book.java
@@ -1,32 +1,26 @@
 package nl.han.se.bewd.springboottest.domein;
 
 public class Book {
-    public String ibsn;
-    public String title;
-    public String author;
+    private String ibsn;
+    private String title;
+    private String author;
 
+    public Book(String ibsn, String title, String author) {
+        this.ibsn = ibsn;
+        this.title = title;
+        this.author = author;
+    }
 
     public String getIbsn() {
         return ibsn;
-    }
-
-    public void setIbsn(String ibsn) {
-        this.ibsn = ibsn;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getAuthor() {
         return author;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
 }
